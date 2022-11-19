94c637810f721c819b1253e552801110eaff0834
supermanisme2002 <bthanh2001@gmail.com>
2022-11-19 16:36:25 +0700

    chapter 4

---
+++ src/Making-React-interactive-with-states/index.js
@@ -0,0 +1,20 @@
+/*
+1 You can set state in a component method (not a constructor) with which syntax? this.setState(a), this.state = a, or this.a = a
+
+this.setState(a)
+
+2 If you want to update the render process, it’s normal practice to change proper-
+ties in components like this: this.props.a=100. True or false?
+false
+3 States are mutable, and properties are immutable. True or false?
+True
+4 Stateless components can be implemented as functions. True or false?
+True
+5 How do you define the first state variables when an element is created?
+setState(), initialState(), this.state =... in the constructor, or
+setInitialState()
+
+this.state = {
+    name : value
+}
+*/
