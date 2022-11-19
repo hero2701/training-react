4ccbda8605dd1d52890537741aaf9a2b491dff75
supermanisme2002 <bthanh2001@gmail.com>
2022-11-19 15:44:31 +0700

    chapter 3

---
+++ src/introduction-to-JSX/index.js
@@ -0,0 +1,22 @@
+/*
+1 To output a JavaScript variable in JSX, which of the following do you use? =, <%=
+%>, {}, or <?= ?>
+
+ {}
+
+2 The class attribute isn’t allowed in JSX. True or false?
+
+true
+
+3 The default value for an attribute without a value is false. True or false?
+False.
+
+4 The inline style attribute in JSX is a JavaScript object and not a string like other
+attributes. True or false?
+True.
+5 If you need to have if/else logic in JSX, you can use it inside {}. For example,
+class={if (!this.props.admin) return 'hide'} is valid JSX code. True or
+false?
+
+false -- class={ !this.props.admin && 'hide'}
+*/
