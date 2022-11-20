29fbc37b608f3d2a830a27f2c5e31e6b9691eb89
supermanisme2002 <bthanh2001@gmail.com>
2022-11-20 19:12:10 +0700

    chapter 6

---
+++ src/handling-events-in-react/index.js
@@ -0,0 +1,27 @@
+/*
+1 Select the correct syntax for the event declaration: onClick=this.doStuff,
+onclick={this.doStuff}, onClick="this.doStuff", onClick={this.doStuff},
+or onClick={this.doStuff()}
+
+ onClick={this.doStuff}
+
+2 componentDidMount() won’t be triggered during server-side rendering of the
+React component on which it’s declared. True or false?
+
+true
+
+3 One way to exchange information among child components is to move the
+object to the parent of the children. True or false?
+
+true
+
+4 You can use event.target asynchronously and outside the event handler by
+default. True or false?
+
+false
+
+5 You can integrate with third-party libraries and events not supported by React
+by setting up event listeners in the component lifecycle events. True or false?
+
+true
+*/
