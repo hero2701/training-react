f3b243b0bfd33cf777615b51a4eb1967346b1486
supermanisme2002 <bthanh2001@gmail.com>
2022-11-22 18:53:43 +0700

    chapter 7

---
+++ src/Working-with-forms-in-React/index.js
@@ -0,0 +1,31 @@
+/*
+
+1 An uncontrolled component sets a value, and a controlled component doesn’t.
+True or false?
+
+False.
+
+2 The correct syntax for default values is which of the following? default-value,
+defaultValue, or defVal
+
+defaultValue
+
+3 The React team recommends using onChange over onInput. True or false?
+4 You set a value for the text area with which of the following? Children, inner
+HTML, or value
+
+true
+
+5 In a form, selected applies to which of the following? <input>, <textarea>, or
+<option>
+
+<option>
+
+6 Which of the following is the best way to extract the DOM node by reference?
+React.findDomNode(this.refs.email), this.refs.email, this.refs.email
+.getDOMNode, ReactDOM.findDOMNode(this.refs.email), or this.refs.email
+.getDomNode
+
+ ReactDOM.findDOMNode(this.refs.email)
+
+*/
